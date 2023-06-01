04b8f6fb334e799b8a18852f5806ef946aef429c
Joauquin Astorga <joa.iog11.23@gmial.com>
2023-06-01 12:16:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cursoviewbinding/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.cursoviewbinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.cursoviewbinding.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+
+}
